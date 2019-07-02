9fa2ed5c8c7eb82110d7f99cc3e51b003d7f2e72
Justin Kerntz and Todd Aden <todd@>
2019-07-02 11:53:24 -0700

    add timewheel

--- src/components/Timewheel/Timewheel.jsx
+++ src/components/Timewheel/Timewheel.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import Wheel from './TimeWheel.png'
 
-export default class Timewheel extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+import { makeStyles } from '@material-ui/core/styles';
+
+let now = new Date();
+let start = new Date(now.getFullYear(), 0, 0);
+let diff = now - start;
+let oneDay = 1000 * 60 * 60 * 24;
+let day = Math.floor(diff / oneDay);
+
+
+const useStyles = makeStyles(theme => ({
+    wheel: {
+      '& img' : {
+        transform: `rotate(${(day * 0.986) - 30}deg)`,
+        transition: '300ms linear all'
+      }
+    }
+}));
+
+export default function Timewheel () {
+  const classes = useStyles();  
+  return (
+        <div className={classes.wheel}>
+        <img src={Wheel} alt=""/>
+      </div>  
+  )
 }
